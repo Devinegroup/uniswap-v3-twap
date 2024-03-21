d7254749dbb9dd6cf8445fb7d8f206271e08bade
Devine Group XYZ <devinegroup@proton.me>
2024-03-21 12:54:01 +0000

    update

--- script/DeployUniswapV3Twap.s.sol
+++ script/DeployUniswapV3Twap.s.sol
@@ -2,19 +2,20 @@
 pragma solidity ^0.7.6;
 
 import "forge-std/Script.sol";
-import "../src/UniswapV3Twap.sol"; // Adjust the path as necessary to where your contract is located
+import "src/contracts/UniswapV3Twap.sol"; // Adjust the path as necessary to where your contract is located
 
 contract DeployUniswapV3Twap is Script {
+
+    
+    address private constant UNISWAP_V3_FACTORY_ADDRESS = 0x0227628f3F023bb0B980b67D528571c95c6DaC1c;
+    address private constant TOKEN0 = 0x6F96d6E2d7EB1F44Ff56eBE2AaB59B20202fbC86;
+    address private constant TOKEN1 = 0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0;
+    uint24 private constant FEE = 3000;
 
     function run() public {
         vm.startBroadcast();
 
-        address factory = /* Factory Address Here */;
-        address token0 = /* Token0 Address Here */;
-        address token1 = /* Token1 Address Here */;
-        uint24 fee = /* Fee Tier Here */;
-
-        UniswapV3Twap uniswapV3Twap = new UniswapV3Twap(factory, token0, token1, fee);
+        UniswapV3Twap uniswapV3Twap = new UniswapV3Twap(UNISWAP_V3_FACTORY_ADDRESS, TOKEN0, TOKEN1, FEE);
         console.log("UniswapV3Twap deployed to:", address(uniswapV3Twap));
 
         vm.stopBroadcast();
